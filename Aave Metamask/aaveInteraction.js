95bd1272091aff27ddd6ff3b7e84a60db16a7934
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-11-23 00:27:19 +0530

    Update aaveInteraction.js

--- Aave Metamask/aaveInteraction.js
+++ Aave Metamask/aaveInteraction.js
@@ -1,24 +1,3 @@
-// async function connectToMetaMask() {
-//     try {
-//         console.log('Attempting to connect to MetaMask...');
-//         if (window.ethereum) {
-//             console.log('MetaMask provider found.');
-//             window.web3 = new Web3(window.ethereum);
-//             const accounts = await window.ethereum.enable();
-//             console.log('Accounts:', accounts);
-//             if (accounts.length > 0) {
-//                 document.getElementById('depositButton').disabled = false;
-//                 console.log('Connected to MetaMask');
-//             } else {
-//                 console.log('Please connect to MetaMask');
-//             }
-//         } else {
-//             console.log('MetaMask not found!');
-//         }
-//     } catch (error) {
-//         console.error('Error connecting to MetaMask:', error);
-//     }
-// }
 document.getElementById('connectButton').onclick = async function () {
     try {
         if (window.ethereum) {
